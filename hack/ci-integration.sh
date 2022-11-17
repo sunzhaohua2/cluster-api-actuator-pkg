7a538e05924e5ebcda1bdc86682bf030e36aec9e
Denis Moiseev <1239415+lobziik@users.noreply.github.com>
2022-11-17 22:45:31 +0100

    Increase global suite timeout
    Observed that 90m is not enough to perform the entire suite
    on Azure, this commit increases this.
    
    Since 2h is a hard limit in OCP CI, set 115m + 5m grace period
    for performing gathering and cleanup.

--- hack/ci-integration.sh
+++ hack/ci-integration.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-go run ./vendor/github.com/onsi/ginkgo/ginkgo \
-    -timeout 90m \
-    -stream \
+go run ./vendor/github.com/onsi/ginkgo/v2/ginkgo \
     -v \
-    --failFast \
-    --noColor \
+    --timeout=115m \
+    --grace-period=5m \
+    --fail-fast \
+    --no-color \
+    --junit-report="junit_cluster_api_actuator_pkg_e2e.xml" \
+    --output-dir="${OUTPUT_DIR}" \
     "$@" \
     ./pkg/ -- --alsologtostderr -v 4 -kubeconfig ${KUBECONFIG:-~/.kube/config}
